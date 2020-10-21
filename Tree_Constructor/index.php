d0624dd8faa59e629ed6d04b57fdecec5c56a436
amryami <amr.anter@esmaar.net>
2020-10-21 03:45:32 +0200

    Tree Constructor

--- Tree_Constructor/index.php
+++ Tree_Constructor/index.php
@@ -1,42 +1,4 @@
 <?php
-
-function TreeConstructor($strArr) {
-
-    // Parents have up to 2 children and children have only one father.
-    // $parents[$i] = [1,2]
-    // $children[$i] = [1]
-
-    $parents = [];
-    $children = [];
-
-    foreach($strArr as $item) {
-        $item = str_replace('(', '', $item);
-        $item = str_replace(')', '', $item);
-        $item = explode(',', $item);
-
-        $parents[$item[1]][] = $item[0];
-        // if parent has more than 2 children it's not a binary tree
-        if (count($parents[$item[1]]) > 2) {
-            return "false";
-        }
-        $children[$item[0]][] = $item[1];
-        if (count($children[$item[0]]) > 1) {
-            return "false";
-        }
-
-    }
-
-
-    // code goes here
-    return "true";
-
-
-}
-
-// keep this function call here
-echo TreeConstructor(["(2,3)", "(1,2)", "(4,9)", "(9,3)", "(12,9)", "(6,4)"]);
-echo '<br><br><br><br><br><br>';
-
 $parents = [];
 $children = [];
 
