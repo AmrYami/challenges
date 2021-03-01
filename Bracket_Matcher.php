e422f36a072e210721110be7b4cc94cabbff176d
amryami1 <amr.anter@esmaar.net>
2021-03-01 23:37:07 +0200

    updates in brackets matcher

--- Bracket_Matcher.php
+++ Bracket_Matcher.php
@@ -2,11 +2,11 @@
 
 function BracketMatcher($str)
 {
-    $string = preg_replace('`()|([^)(]+)`','',$str);//remove all chars without what we need
+    $string = preg_replace('`()|([^)(]+)`', '', $str);//remove all chars without what we need
     $result = RemoveBrackets($string);// send to RemoveBrackets
     if (strlen($result) > 0)
         return '0';
-     else
+    else
         return '1';
 }
 
@@ -21,3 +21,62 @@ function RemoveBrackets($string)
 
 // keep this function call here
 echo BracketMatcher('fge ts(fop en(p hp2 ( 34 aa d://st din, r))');
+
+echo '<br>';
+
+
+echo main('[({[({[({[({[({[({})]})]})]})]})]})]');
+
+function main($A)
+{
+
+    $stack = new Stack();
+    return $stack->matcher($A);
+
+}
+
+class Stack
+{
+private $count = 0;
+private $turns = 6;
+    public function matcher($str)
+    {
+        $res = $this->bracketsMatcher($str, '`()|([^)(]+)`', '/\(\)/');
+        if ($this->count > $this->turns)
+            return 'too match brackets';
+        $res1 = $this->bracketsMatcher($str, '`()|([^}{]+)`', '/\{\}/');
+        if ($this->count > $this->turns)
+            return 'too match brackets';
+        $res2 = $this->bracketsMatcher($str, '`()|([^][]+)`', '/\[\]/');
+        if ($this->count > $this->turns)
+            return 'too match brackets';
+
+        if ($res && $res1 && $res2)
+            return 'Successfully';
+        else
+            return 'Failed';
+    }
+
+    function bracketsMatcher($str, $pattenr, $find)
+    {
+        $this->count = 0;
+        $string = preg_replace($pattenr, '', $str);//remove all chars without what we need
+        $result = $this->RemoveBrackets($string, $find);// send to RemoveBrackets
+        if ($result === "too match brackets")
+            return 'too match brackets';
+        elseif (strlen($result) > 0)
+            return false;
+        else
+            return true;
+    }
+
+    function RemoveBrackets($string, $find)
+    {
+        $this->count = $this->count + 1;
+        if (preg_match($find, $string))// if string has what we need
+            $string = preg_replace($find, '', $string);// remove chars from string
+        if (preg_match($find, $string))// if string has what we need
+            $string = $this->RemoveBrackets($string, $find); // recursive
+        return $string;
+    }
+}
